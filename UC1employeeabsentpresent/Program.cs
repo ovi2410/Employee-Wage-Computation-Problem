7c77a311e47fdbd9598d76c38cce822d4a656cd9
ovi2410 <guneshwarifegade@gmail.com>
2022-04-28 14:34:05 +0530

    presentorabsent

---
+++ UC1employeeabsentpresent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace employeeabsentpresent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random obj = new Random();
+            int empCheck = obj.Next(0,2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("employeee is present");
+            else
+                Console.WriteLine("employee is absent");
+            Console.ReadKey();
+        }
+    }
+}
